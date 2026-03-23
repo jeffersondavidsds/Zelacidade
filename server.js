f60f50c88e3861da534025bdf994cf0e5084a52b
Jefferson David <jeffersondavidsd@gmail.com>
2026-03-23 10:33:42 -0300

    adição de busca por ID

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
         <h2>Gestão de Problemas Urbanos</h2>
         <p>Endpoint que leva aos incidentes cadastrados: /incidentes</p>
         <button onclick="window.location.href='/incidentes'">Ver Incidentes</button>
+               
         
         </body>
         `)
@@ -33,4 +34,26 @@ const PORT = 3000
 app.listen(PORT, async () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`)
 
+})
+
+
+app.get('/incidentes/:id', async (req, res) => {
+    const { id } = req.params
+    const db = await criarBanco() // Espera o acesso ao Banco
+    const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id]) // Consulta o SQL para selecionar o incidente específico com baso no ID fornecido )
+    res.json(incidenteEspecifico) // Envia o incidente específico como resposta Json
+})
+
+// ROTA POST: Define uma rota do tipo POST para endpoint '/incidentes'
+
+
+    app.post('/incidentes', async (req, res) => {
+    const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema} = req.body
+
+    const db = await criarBanco()
+    await db.run(`INSERT INTO incidentes (tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema])
+    
+    res.send(`Incidente novo registrado: ${tipo_problema} no nome de ${nome_solicitante} Telefone ${contato_solicitante} na data ${data_registro} e na hora ${hora_registro} - ${imagem_problema}`)
+    
 })
